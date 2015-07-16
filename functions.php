abf5f6cbc3d65473d4dcca2556937c0d7009b278
Jeremy Bass <jeremybass@cableone.net>
2015-07-16 15:32:00 -0700

    start out the base routes for the json feed endpoints

--- functions.php
+++ functions.php
@@ -4,6 +4,59 @@ class WSU_MurrowConnect_Theme {
 	 * Setup the hooks used in the theme.
 	 */
 	public function __construct() {
+		
 	}
 }
 new WSU_MurrowConnect_Theme();
+
+function murrow_api_init() {
+	global $Murrow_api_bp_user;
+	$murrow_api_bp_user = new Murrow_API_BP_USER();
+	add_filter( 'json_endpoints', array( $murrow_api_bp_user, 'register_routes' ) );
+}
+add_action( 'wp_json_server_before_serve', 'murrow_api_init' );
+
+class Murrow_API_BP_USER {
+	/**
+	* Register Routes
+	*/
+	public function register_routes( $routes ) {
+		$routes['/bp/users'] = array(
+			array( array( $this, 'get_users'), WP_JSON_Server::READABLE )
+		);
+		$routes['/bp/user/(?P<id>\d+)'] = array(
+			array( array( $this, 'get_user'), WP_JSON_Server::READABLE )
+		);
+		// Add more custom routes here
+		return $routes;
+	}
+	
+	public function get_users(){
+		
+		$args = array(
+			//params?
+		);
+		
+		// The Query
+		$user_query = new WP_User_Query( $args );
+		
+		// User Loop
+		if ( ! empty( $user_query->results ) ) {
+			foreach ( $user_query->results as $user ) {
+				//$some_field = xprofile_get_field_data( 'Some Field', $user->id );
+				var_dump( $user );
+			}
+		} else {
+			echo 'No users found.';
+		}/**/
+
+		
+		
+		var_dump( 'end of user list' );die();
+	}
+	
+	public function get_user(){
+		var_dump('a user');die();
+	}
+}
+
